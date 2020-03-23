80aa40f0b746918d3e33c7342eadefad194fb11c
Ted Kesgar <t.kesgar@live.com>
2020-03-24 06:07:04 +0700

    Update dependencies, remove next-sass, only support next>=9

--- packages/sharo-next/index.js
+++ packages/sharo-next/index.js
@@ -1,16 +1,12 @@
-const withSass = require('@zeit/next-sass')
-
 /**
  * This function is a Next.js plugin for sharo.
  *
  * Features:
  *   - MDX support via `@zeit/next-mdx`
- *   - SASS/SCSS support via `@zeit/next-sass`
  *
  * Docs:
  *   - https://nextjs.org/docs#custom-configuration
  *   - https://www.npmjs.com/package/@zeit/next-mdx
- *   - https://www.npmjs.com/package/@zeit/next-sass
  *
  * @param {any} nextConfig Next.js configuration object
  * @returns {any} New Next.js configuration object
@@ -18,41 +14,38 @@ const withSass = require('@zeit/next-sass')
 function withSharo(nextConfig = {}) {
   // https://github.com/zeit/next-plugins/issues/320
   const withMdx = require('@zeit/next-mdx')({
-  // Allow regular markdown files (*.md) to be imported.
+    // Allow regular markdown files (*.md) to be imported.
     extension: /\.mdx?$/
   })
 
-  return (
-    withSass(withMdx(
-      Object.assign(
-        // =====================================================================
-        // Default configurations (can be overridden by nextConfig)
-        // =====================================================================
-        {
-          // Currently empty
-        },
-        // =====================================================================
-        // Override default configurations with nextConfig
-        // =====================================================================
-        nextConfig,
-        // =====================================================================
-        // Override nextConfig configurations
-        // (note to self: follow Next.js rules on this section)
-        // =====================================================================
-        {
-          webpack(config, options) {
-          // Allow autoresolving of MDX (*.md, *.mdx) and SCSS (*.scss, *.sass)
-            config.resolve.extensions.push('.md', '.mdx', '.scss', '.sass')
+  return withMdx(
+    Object.assign(
+      // =====================================================================
+      // Default configurations (can be overridden by nextConfig)
+      // =====================================================================
+      {
+        // Currently empty
+      },
+      // =====================================================================
+      // Override default configurations with nextConfig
+      // =====================================================================
+      nextConfig,
+      // =====================================================================
+      // Override nextConfig configurations
+      // (note to self: follow Next.js rules on this section)
+      // =====================================================================
+      {
+        webpack(config, options) {
+          config.resolve.extensions.push('.md', '.mdx')
 
-            if (typeof nextConfig.webpack === 'function') {
-              return nextConfig.webpack(config, options)
-            }
-
-            return config
+          if (typeof nextConfig.webpack === 'function') {
+            return nextConfig.webpack(config, options)
           }
+
+          return config
         }
-      )
-    ))
+      }
+    )
   )
 }
 
