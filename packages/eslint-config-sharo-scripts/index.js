2e0254fb4273fa06c4620a85bbb45e2ae221d7ad
Ted Kesgar <t.kesgar@live.com>
2020-04-04 05:19:38 +0700

    Style formatting

--- packages/eslint-config-sharo-scripts/index.js
+++ packages/eslint-config-sharo-scripts/index.js
@@ -1,54 +1,48 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'prettier/react'
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
   ],
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
   env: {
-    browser: true
+    browser: true,
   },
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   overrides: [
     {
-      files: '**/*.{ts,tsx}',
-      parser: '@typescript-eslint/parser',
-      plugins: [
-        '@typescript-eslint'
-      ],
+      files: "**/*.{ts,tsx}",
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
       extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
-        'prettier/@typescript-eslint',
-        'prettier/react'
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint",
+        "prettier/react",
       ],
       settings: {
         react: {
-          version: 'detect'
-        }
-      }
+          version: "detect",
+        },
+      },
     },
     {
-      files: 'cypress/**/*.js',
-      plugins: [
-        'chai-friendly'
-      ],
-      extends: [
-        'plugin:cypress/recommended'
-      ],
+      files: "cypress/**/*.js",
+      plugins: ["chai-friendly"],
+      extends: ["plugin:cypress/recommended"],
       rules: {
-        'chai-friendly/no-unused-expressions': 'error'
-      }
-    }
-  ]
-}
+        "chai-friendly/no-unused-expressions": "error",
+      },
+    },
+  ],
+};
