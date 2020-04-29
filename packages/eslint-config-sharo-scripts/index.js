dbf084b3ceef458edb68b30f5df5d6ffc444e15f
Ted Kesgar <t.kesgar@live.com>
2020-04-29 22:48:28 +0700

    Fix formatting

--- packages/eslint-config-sharo-scripts/index.js
+++ packages/eslint-config-sharo-scripts/index.js
@@ -1,53 +1,53 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jest/recommended",
-    "plugin:jest/style",
-    "plugin:prettier/recommended",
-    "prettier/react",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:prettier/recommended',
+    'prettier/react'
   ],
   settings: {
     react: {
-      version: "detect",
-    },
+      version: 'detect'
+    }
   },
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   overrides: [
     {
-      files: "**/*.{ts,tsx}",
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint"],
+      files: '**/*.{ts,tsx}',
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
       extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:jest/recommended",
-        "plugin:jest/style",
-        "plugin:prettier/recommended",
-        "prettier/@typescript-eslint",
-        "prettier/react",
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:jest/recommended',
+        'plugin:jest/style',
+        'plugin:prettier/recommended',
+        'prettier/@typescript-eslint',
+        'prettier/react'
       ],
       settings: {
         react: {
-          version: "detect",
-        },
-      },
+          version: 'detect'
+        }
+      }
     },
     {
-      files: "cypress/**/*.js",
-      plugins: ["chai-friendly"],
-      extends: ["plugin:cypress/recommended"],
+      files: 'cypress/**/*.js',
+      plugins: ['chai-friendly'],
+      extends: ['plugin:cypress/recommended'],
       rules: {
-        "chai-friendly/no-unused-expressions": "error",
-      },
-    },
-  ],
-};
+        'chai-friendly/no-unused-expressions': 'error'
+      }
+    }
+  ]
+}
