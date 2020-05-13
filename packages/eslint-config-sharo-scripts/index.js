3dac0611c008739175a19eb2628f99ca26da8810
Ted Kesgar <t.kesgar@live.com>
2020-05-14 00:03:34 +0700

    Add eslint-plugin-jsx-a11y

--- packages/eslint-config-sharo-scripts/index.js
+++ packages/eslint-config-sharo-scripts/index.js
@@ -3,9 +3,24 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
     'prettier/react'
   ],
+  rules: {
+    /**
+     * This is a workaround for Next.js Link API issue:
+     * https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+     */
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton']
+      }
+    ]
+  },
   settings: {
     react: {
       version: 'detect'
