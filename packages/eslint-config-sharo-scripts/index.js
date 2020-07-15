5e7b925f7649739406149627458a683dfb299df7
Ted Kesgar <t.kesgar@live.com>
2020-07-16 06:13:59 +0700

    Add missing jsx-a11y extends for ts/tsx files

--- packages/eslint-config-sharo-scripts/index.js
+++ packages/eslint-config-sharo-scripts/index.js
@@ -44,12 +44,27 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
         'plugin:jest/recommended',
         'plugin:jest/style',
         'plugin:prettier/recommended',
         'prettier/@typescript-eslint',
         'prettier/react'
       ],
+      rules: {
+        /**
+         * This is a workaround for Next.js Link API issue:
+         * https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+         */
+        'jsx-a11y/anchor-is-valid': [
+          'error',
+          {
+            components: ['Link'],
+            specialLink: ['hrefLeft', 'hrefRight'],
+            aspects: ['invalidHref', 'preferButton']
+          }
+        ]
+      },
       settings: {
         react: {
           version: 'detect'
