86ea0d6f35b0673c3a3908ca5abbf2fe09f154ef
Ted Kesgar <t.kesgar@hey.com>
2021-02-15 06:27:30 +0700

    Update styled-jsx-plugin-postcss to v4

--- packages/sharo-babel/index.js
+++ packages/sharo-babel/index.js
@@ -1,6 +1,7 @@
 module.exports = () => ({
   presets: [
     ['next/babel', {
+      // https://github.com/vercel/styled-jsx#css-preprocessing-via-plugins
       'styled-jsx': {
         plugins: ['styled-jsx-plugin-postcss']
       }
