312c11ab6ec1e8772cffa97264818a318108aeb0
Ted Kesgar <t.kesgar@outlook.com>
2021-06-27 18:32:31 +0700

    eslint: add env browser and node

--- packages/eslint-config-sharo-scripts/index.js
+++ packages/eslint-config-sharo-scripts/index.js
@@ -1,5 +1,9 @@
 module.exports = {
   extends: ['next', 'eslint:recommended', 'plugin:prettier/recommended'],
+  env: {
+    browser: true,
+    node: true
+  },
   overrides: [
     {
       files: 'cypress/**/*.js',
